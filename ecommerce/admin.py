94ab770dbef73329b377e1245f5b294d5fde8fab
Sameer Shahzad <143422935+Student-ITU@users.noreply.github.com>
2025-10-27 23:54:15 +0500

    second commit

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from django.contrib.auth.admin import userAdmin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('city', 'phone')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Info', {'fields': ('city', 'state', 'address', 'phone')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
