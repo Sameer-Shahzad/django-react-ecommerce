117bbc56ece8afb6795634557f750638dcc9e39d
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-10-28 00:42:36 +0500

    fourth commit

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ecommerce',
     'shop',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Product(models.Model):
+    CATEGORY = (('Electronics', 'ELECTRONICS'),
+                ('Clothing', 'CLOTHING'),
+                ('Groceries', 'GROCERIES'),
+    )
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(upload_to="img")
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-
+    category = models.CharField(choices=CATEGORY, max_length=50, blank=True, null=True)
     def __str__(self):
         return self.name
+    
+    
+
+def save(self, *args, **kwargs):
+    if not self.id and not self.slug: 
+        base_slug = slugify(self.name) 
+        unique_slug = base_slug
+        counter = 1
+        
+        while Product.objects.filter(slug=unique_slug).exists():
+            unique_slug = f'{base_slug}-{counter}' 
+            counter += 1
+            
+        self.slug = unique_slug 
+        
+    super().save(*args, **kwargs)
